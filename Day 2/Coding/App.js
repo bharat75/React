cde198f4acd0c03882228c00bf1856645b39a8f3
BharatShubham <bharatshubham75@gmail.com>
2024-07-21 15:00:26 +0530

    Recent push

--- Day 2/Coding/App.js
+++ Day 2/Coding/App.js
@@ -1,4 +1,19 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello";
-const root = document.getElementById("root");
-root.appendChild(heading);
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading1 = React.createElement(
+  "h1",
+  { id: "Heading1" },
+  "This my first heading"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "Heading2" },
+  "This my second heading"
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
