4f910c93caffd3ff99e035c6ebd2e7b1e09791c6
BharatShubham <bharatshubham75@gmail.com>
2024-07-26 22:39:54 +0530

    Nothing personal

--- Day3/App.js
+++ Day3/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  { id: "heading1", key: "h1" },
-  "Heading 1"
-);
-const heading2 = React.createElement(
-  "h2",
-  { id: "heading2", key: "h2" },
-  "Heading 2"
+const heading = (
+  <h1 id="heading1" key="h1">
+    Heading 1
+  </h1>
 );
 
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+const Title = () => {
+  return <h3>Hello Dude</h3>;
+};
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      {Title()}
+      {heading}
+      <h2>Heading2 namaste</h2>
+      <h2>Heading2 react</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(container);
+//root.render(heading);
+root.render(<HeaderComponent />);
