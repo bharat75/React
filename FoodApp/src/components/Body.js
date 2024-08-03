7c8f68170b7f445a1acd79b34e2876ece755340a
BharatShubham <bharatshubham75@gmail.com>
2024-08-03 17:30:46 +0530

    Search Functionality

--- FoodApp/src/components/Body.js
+++ FoodApp/src/components/Body.js
@@ -1,16 +1,50 @@
 import { restaurantList } from "../config.js";
 import RestrauntCard from "./RestrauntCard.js";
+import { useState } from "react";
+
+function filterData(searchtxt, restraunts) {
+  return restraunts.filter((restraunt) => {
+    restraunt.data.name.includes(searchtxt);
+  });
+}
 
 const BodyComponent = () => {
+  //const searchtxt = "";
+  //const abc = "hello";
+  //const [abc] = useState("hello");
+  const [searchtxt, setSearchText] = useState("");
+  const [restraunts, setrestraunts] = useState(restaurantList);
   return (
-    <div className="listing">
-      {/* <RestrauntCard {...restaurantList[0].data} /> */}
-      {restaurantList.map((listingCards) => {
-        return (
-          <RestrauntCard {...listingCards.data} key={listingCards.data.id} />
-        );
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by name, cuisine, or location"
+          className="search-input"
+          value={searchtxt}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchtxt, restraunts);
+            setrestraunts(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="listing">
+        {/* <RestrauntCard {...restaurantList[0].data} /> */}
+        {restraunts.map((listingCards) => {
+          return (
+            <RestrauntCard {...listingCards.data} key={listingCards.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
