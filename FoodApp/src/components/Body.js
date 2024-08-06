f1a8a5e1f42d97f7fc85797037ccd406699e860b
BharatShubham <bharatshubham75@gmail.com>
2024-08-07 00:48:53 +0530

    Called API

--- FoodApp/src/components/Body.js
+++ FoodApp/src/components/Body.js
@@ -1,11 +1,13 @@
 import { restaurantList } from "../config.js";
 import RestrauntCard from "./RestrauntCard.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer.js";
 
 function filterData(searchtxt, restraunts) {
-  return restraunts.filter((restraunt) => {
-    restraunt.data.name.includes(searchtxt);
-  });
+  const filterData = restraunts.filter((restraunt) =>
+    restraunt.data.name.includes(searchtxt)
+  );
+  return filterData;
 }
 
 const BodyComponent = () => {
@@ -13,8 +15,31 @@ const BodyComponent = () => {
   //const abc = "hello";
   //const [abc] = useState("hello");
   const [searchtxt, setSearchText] = useState("");
-  const [restraunts, setrestraunts] = useState(restaurantList);
-  return (
+  const [filteredRestraunts, setFilteredRestraunts] = useState([]);
+  const [allRestraunts, setAllRestraunts] = useState([]);
+
+  useEffect(() => {
+    //API Call
+    getRestrauntListing();
+  }, []);
+
+  async function getRestrauntListing() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5279995&lng=88.3660445&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    const fetchedRestraunts =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || [];
+    console.log(fetchedRestraunts);
+    setAllRestraunts(fetchedRestraunts);
+    setFilteredRestraunts(fetchedRestraunts);
+  }
+
+  return filteredRestraunts.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -29,8 +54,8 @@ const BodyComponent = () => {
         <button
           className="search-btn"
           onClick={() => {
-            const data = filterData(searchtxt, restraunts);
-            setrestraunts(data);
+            const data = filterData(searchtxt, allRestraunts);
+            setFilteredRestraunts(data);
           }}
         >
           Search
@@ -38,9 +63,9 @@ const BodyComponent = () => {
       </div>
       <div className="listing">
         {/* <RestrauntCard {...restaurantList[0].data} /> */}
-        {restraunts.map((listingCards) => {
+        {filteredRestraunts.map((listingCards) => {
           return (
-            <RestrauntCard {...listingCards.data} key={listingCards.data.id} />
+            <RestrauntCard {...listingCards.info} key={listingCards.info.id} />
           );
         })}
       </div>
